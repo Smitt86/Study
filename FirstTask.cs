58c0401790158795513b51baa6f9794fa189beea
Smitt86 <pavlov1086@ya.ru>
2023-06-12 18:42:59 +0300

    last task

--- FirstTask.cs
+++ FirstTask.cs
@@ -1,16 +1,10 @@
-﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.Clear();
-Console.WriteLine("Ищем большее значение из трех чисел");
-
-Console.WriteLine("Введите первое число");
-int first = int.Parse (Console.ReadLine());
-Console.WriteLine ("Введите второе число");
-int second = int.Parse (Console.ReadLine());
-Console.WriteLine ("Введите третье число");
-int third = int.Parse (Console.ReadLine());
-
-int max = first;
-if (first > max) max = first;
-if (second > max) max = second;
-if (third > max) max = third;
-Console.WriteLine ($"Максимальное число = {max}");
+Console.WriteLine ("Введите число N");
+int N = int.Parse (Console.ReadLine());
+int i = 2;
+Console.WriteLine ("Чётные числа от 1 до N:");
+while (i<=N) {
+    if (i%2==0) Console.WriteLine (i);
+    i++;
+}
