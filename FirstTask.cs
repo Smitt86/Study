b2cf936cd328706360551d5a1972cdb85f1bec33
Smitt86 <pavlov1086@ya.ru>
2023-06-12 18:27:21 +0300

    second task

--- FirstTask.cs
+++ FirstTask.cs
@@ -1,16 +1,16 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Clear();
-Console.WriteLine("Ищем большее значение из двух чисел");
+Console.WriteLine("Ищем большее значение из трех чисел");
+
 Console.WriteLine("Введите первое число");
 int first = int.Parse (Console.ReadLine());
 Console.WriteLine ("Введите второе число");
 int second = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите третье число");
+int third = int.Parse (Console.ReadLine());
 
-if (first > second)
-{
-    Console.WriteLine($"Большее значение = {first} Меньшее значение = {second}");
-}   
-else 
-{
-    Console.Write($"Большее значение = {second} Меньшее значение = {first}");   
-}
+int max = first;
+if (first > max) max = first;
+if (second > max) max = second;
+if (third > max) max = third;
+Console.WriteLine ($"Максимальное число = {max}");
